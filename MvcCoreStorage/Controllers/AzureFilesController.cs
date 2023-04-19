347eb3e7e1b0181031ebec8389d11333117fb958
xiangcitoZ <pzxiang1@gmail.com>
2023-04-19 12:36:00 +0200

    Add project files.

---
+++ MvcCoreStorage/Controllers/AzureFilesController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreStorage.Services;
+
+namespace MvcCoreStorage.Controllers
+{
+    public class AzureFilesController : Controller
+    {
+        private ServiceStorageFiles service;
+
+        public AzureFilesController(ServiceStorageFiles service) 
+        {
+            this.service = service;
+        
+        }
+
+        public async Task<IActionResult> Index()
+        {   
+            List<string> files = await this.service.GetFilesAsync();
+            return View(files);
+        }
+
+        public async Task<IActionResult> ReadFile(string filename)
+        {   
+            string data = await this.service.ReadFileAsync(filename);
+            ViewData["DATA"] = data;
+            return View();
+        }
+
+
+        public async Task<IActionResult> DeleteFile(string filename)
+        {
+            await this.service.DeleteFilesAsync(filename);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult UploadFile()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UploadFile(IFormFile file)
+        {
+            string fileName = file.FileName;
+            using(Stream stream = file.OpenReadStream()) 
+            {
+                await this.service.UploadFileAsync(fileName, stream);
+
+            }
+            ViewData["MENSAJE"] = "Fichero subido correctamente";
+            return View();
+            
+        }
+
+    }
+}
---
+++ MvcCoreStorage/Services/ServiceStorageFiles.cs
@@ -0,0 +1,60 @@
+﻿using Azure.Storage.Files.Shares;
+using Azure.Storage.Files.Shares.Models;
+
+namespace MvcCoreStorage.Services
+{
+    public class ServiceStorageFiles
+    {
+        private ShareDirectoryClient root;
+
+        public ServiceStorageFiles(IConfiguration configuration)
+        {
+            string azureKeys =
+                configuration.GetValue<string>("AzureKeys:StorageAccount");
+            ShareClient shareClient =
+                new ShareClient(azureKeys, "ejemplosfiles");
+            this.root = shareClient.GetRootDirectoryClient();
+        }
+
+        public async Task<List<string>> GetFilesAsync()
+        {
+            List<string> files = new List<string>();
+
+            await foreach(ShareFileItem item in 
+                this.root.GetFilesAndDirectoriesAsync()) 
+            {
+                files.Add(item.Name);
+            }
+            return files;
+        }
+            
+        public async Task<string> ReadFileAsync(string fileName)
+        {
+            ShareFileClient file = this.root.GetFileClient(fileName);
+            ShareFileDownloadInfo data = await file.DownloadAsync();
+            Stream stream = data.Content;
+            string contenido = "";
+            using(StreamReader reader = new StreamReader(stream)) 
+            {
+                contenido = await reader.ReadToEndAsync();
+            }
+            return contenido;
+        }
+
+        public async Task UploadFileAsync(string fileName,
+            Stream stream)
+        {
+            ShareFileClient file = this.root.GetFileClient(fileName);
+
+            await file.CreateAsync(stream.Length);
+            await file.UploadAsync(stream);
+
+        }
+
+        public async Task DeleteFilesAsync(string fileName)
+        {
+            ShareFileClient file = this.root.GetFileClient(fileName);
+            await file.DeleteAsync();
+        }
+    }
+}
