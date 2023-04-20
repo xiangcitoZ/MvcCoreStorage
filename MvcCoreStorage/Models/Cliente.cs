114c74a09d96587ce43ebc63dc16b6c10dbaa21d
xiangcitoZ <pzxiang1@gmail.com>
2023-04-20 10:01:39 +0200

    x

---
+++ MvcCoreStorage/Models/Cliente.cs
@@ -0,0 +1,39 @@
+﻿using Azure;
+using Azure.Data.Tables;
+
+namespace MvcCoreStorage.Models
+{
+    public class Cliente : ITableEntity
+    {
+        public string Nombre { get; set; }
+        public int Salario { get; set; }    
+        public int Edad { get; set; }
+        private int _IdCliente;
+        public int IdCliente 
+        { 
+            get{
+                return this._IdCliente;
+            }
+
+          set{
+                this._IdCliente = value;
+                this.RowKey = value.ToString();
+            }
+        }
+
+        private string _Empresa;
+
+        public string Empresa
+        {
+            get { return this._Empresa; }
+            set { 
+                this._Empresa = value;
+                this.PartitionKey = value;
+            }
+        }
+        public string PartitionKey { get ; set ; }
+        public string RowKey { get ; set ; }
+        public DateTimeOffset? Timestamp { get ; set; }
+        public ETag ETag { get ; set ; }
+    }
+}
---
+++ MvcCoreStorage/Services/ServiceStorageTables.cs
@@ -0,0 +1,63 @@
+﻿using Azure.Data.Tables;
+using MvcCoreStorage.Models;
+
+namespace MvcCoreStorage.Services
+{
+    public class ServiceStorageTables
+    {
+        private TableClient tableCliente;
+
+        public ServiceStorageTables(TableServiceClient tableService)
+        {
+            this.tableCliente = tableService.GetTableClient("clientes");
+
+            Task.Run(async () =>
+            {
+                await this.tableCliente.CreateIfNotExistsAsync();
+            });
+        }
+
+        public async Task
+            CreateClientAsync(int id, string nombre, int salario
+            , int edad, string empresa)
+        {
+            Cliente cliente = new Cliente();
+            cliente.IdCliente = id;
+            cliente.Nombre = nombre;
+            cliente.Salario = salario;
+            cliente.Edad = edad;
+            cliente.Empresa = empresa;
+            await this.tableCliente.AddEntityAsync<Cliente>(cliente);
+        }
+
+        public async Task<Cliente> FindClienteAsync
+            (string partitionKey, string rowkey)
+        {
+            Cliente cliente = await 
+                this.tableCliente.GetEntityAsync<Cliente>
+                (partitionKey, rowkey);
+            return cliente;
+        }
+
+        public async Task DeleteClienteAsync
+            (string partitionKey, string rowkey)
+        {
+            await this.tableCliente.DeleteEntityAsync
+                (partitionKey, rowkey);
+        }
+
+        public async Task<List<Cliente>> GetClientesAsync()
+        {
+            List<Cliente> clientes = new List<Cliente>();
+            var query = 
+                this.tableCliente.QueryAsync<Cliente>
+                (filter: "");
+            await foreach(Cliente item in query)
+            {
+                clientes.Add(item);
+            }
+            return clientes;
+        }
+
+    }
+}
